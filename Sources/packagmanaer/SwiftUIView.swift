3705f3825489118df7f77ea5496cda425ac99da9
Shivam Yadav <shivam.yadav@rapipay.com>
2022-09-15 12:01:05 +0530

    First Commit message

---
+++ Sources/packagmanaer/SwiftUIView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shivam Yadav on 15/09/22.
+//
+
+import Foundation
+import SwiftUI
+@available(iOS 13.0, *)
+
+struct SwiftUIVView: View {
+    var body: some View {
+        Text("This Is Thde Shivam yadav Text")
+    }
+}
+
--- Sources/packagmanaer/packagmanaer.swift
+++ Sources/packagmanaer/packagmanaer.swift
@@ -1,3 +1,5 @@
+import Starscream
+
 public struct packagmanaer {
     public private(set) var text = "Hello, World!"
 
