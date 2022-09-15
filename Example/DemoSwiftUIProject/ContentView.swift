4c80d6bf512ebacb1ba8803e3f542c7c17502623
Shivam Yadav <shivam.yadav@rapipay.com>
2022-09-15 14:40:47 +0530

    added some files

---
+++ Example/DemoSwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSwiftUIProject
+//
+//  Created by Shivam Yadav on 15/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/DemoSwiftUIProject/DemoSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIProjectApp.swift
+//  DemoSwiftUIProject
+//
+//  Created by Shivam Yadav on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
